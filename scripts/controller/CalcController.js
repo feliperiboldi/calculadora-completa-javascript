f621c1064766789517502e679a81df017fd47413
Felipe Riboldi <feliperiboldimarques@outlook.com>
2019-01-07 16:28:31 -0200

    Adicionando Evento de Clique nos Botões e Pegando o valor dos Botões

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._currentDate;
 
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
@@ -20,6 +21,12 @@ class CalcController {
 
     initButtonsEvents() {
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e => {
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+        });
     }
 
     setDisplayDateTime() {
