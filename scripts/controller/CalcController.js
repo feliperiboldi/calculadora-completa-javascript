fc534181bf360ea9052ae1df3d1f3d4062adb557
feliperiboldi <felipe.riboldi@daksa.com.br>
2019-02-17 19:22:58 -0300

    Validando último operador e calculando resultado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -46,21 +46,44 @@ class CalcController {
         return (['+', '-', '*', '/', '%'].indexOf(value) > - 1);
     }
 
+    pushOperation(value) {
+        this._operation.push(value);
+
+        if(this._operation.length > 3) {
+            this.calc();
+        }
+    }
+
+    calc() {
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""));
+        
+        this._operation = [result, last];
+    }
+
+    setLastNumberToDisplay() {
+        
+    }
+
     addOperation(value) {
         if(isNaN(this.getLastOperation())) {
                 if(this.isOperator(value)) {
                     this.setLastOperator(value);
                 } else if(isNaN(value)) {
-                    console.log(value);
+                    console.log('Outra coisa', value);
                 } else {
-                    this._operation.push(value);
+                   this.pushOperation(value);
                 }
         } else {
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperator(parseInt(newValue));
-        }
+            if(this.isOperator(value)) {
+                this.pushOperation(value);
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperator(parseInt(newValue));
 
-        console.log(this._operation);
+                this.setLastNumberToDisplay();
+            }
+        }
     }
 
     setError() {
